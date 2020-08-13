ee492b37bd3de6353dfd3b81d12539b903b6ff92
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-13 13:30:34 -0300

    some changes on components

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
@@ -14,6 +15,50 @@ const Dashboard: React.FC = () => {
           <input placeholder="Digite o nome do repositório" />
           <button type="submit">Pesquisar</button>
         </Form>
+
+        <Repositories>
+          <a href="teste">
+            <img src="https://avatars0.githubusercontent.com/u/54296382?s=460&u=878df69a7d6134f4a7100fb85392e7516d0d6cbb&v=4" alt="Felipe Guimarães"/>
+          
+            <div>
+              <strong>felipe2g/e-commerce-price-checker</strong>
+              <p>Check our prices with us!</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+          <a href="teste">
+            <img src="https://avatars0.githubusercontent.com/u/54296382?s=460&u=878df69a7d6134f4a7100fb85392e7516d0d6cbb&v=4" alt="Felipe Guimarães"/>
+          
+            <div>
+              <strong>felipe2g/e-commerce-price-checker</strong>
+              <p>Check our prices with us!</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+          <a href="teste">
+            <img src="https://avatars0.githubusercontent.com/u/54296382?s=460&u=878df69a7d6134f4a7100fb85392e7516d0d6cbb&v=4" alt="Felipe Guimarães"/>
+          
+            <div>
+              <strong>felipe2g/e-commerce-price-checker</strong>
+              <p>Check our prices with us!</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+          <a href="teste">
+            <img src="https://avatars0.githubusercontent.com/u/54296382?s=460&u=878df69a7d6134f4a7100fb85392e7516d0d6cbb&v=4" alt="Felipe Guimarães"/>
+          
+            <div>
+              <strong>felipe2g/e-commerce-price-checker</strong>
+              <p>Check our prices with us!</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+          
+        </Repositories>
 
       </>
     );
